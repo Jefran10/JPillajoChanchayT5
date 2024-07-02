b6856d890f4c065dade081f2b719fb0c2b90eda0
Jefran10 <jefran1999@gmail.com>
2024-07-02 15:55:00 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JPillajoChanchayT5
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
